9becfc0565bfbf98cba6392e94332bb4d9fab284
Humco <telnov-ilya08@mail.ru>
2025-10-06 21:15:53 +0700

    Added !!!

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     calc.add_income(500)
     calc.add_expense(300)
 
-    print("==== Finance Calculator! ====")
+    print("==== Finance Calculator!!! ====")
     print(f"Current balance: {calc.get_balance()}")
 
 if __name__ == "__main__":
